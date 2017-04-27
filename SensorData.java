12dd75f72f69f30ea88cbc8d74eda8b98ce81969
Harry Baines <harryb0905@googlemail.com>
2017-04-27 21:23:35 +0100

    Added support for only allowing CSV files to be opened

--- SensorData.java
+++ SensorData.java
@@ -18,7 +18,6 @@ public class SensorData
 {
 	// Calendar/Date instance variables
 	private static final SimpleDateFormat dateFormat = new SimpleDateFormat("EE MMM dd HH:mm:ss zzz yyyy", Locale.UK);
-	private static final TimeZone timeZone = TimeZone.getTimeZone("UTC");
     private GregorianCalendar cal;
 
     // Data and file variables
@@ -45,43 +44,57 @@ public class SensorData
 		// Swing component = file request dialog
 		source = new JFileChooser();
 		filter = new FileNameExtensionFilter("Comma Seperated Files", "csv"); 
+		source.setAcceptAllFileFilterUsed(false);
 		source.setFileFilter(filter);
+
+		
 
 		if (source.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) 
 		{   
 			selectedFile = source.getSelectedFile();
-			try 
+
+			String fileString = selectedFile.toString();
+
+			String extension = fileString.substring(fileString.lastIndexOf(".") + 1, fileString.length());
+
+			if (!extension.equals("csv")) {
+			    JOptionPane.showMessageDialog(new JFrame(), "Error - please choose a CSV file!", "Error", JOptionPane.ERROR_MESSAGE);
+			}
+			else
 			{
-				reader = new BufferedReader(new FileReader(selectedFile));
-	            while ((line = reader.readLine()) != null) 
-	            {
-	                // Comma = separator
-	                dataLine = line.split(",");
+				try 
+				{
+					reader = new BufferedReader(new FileReader(selectedFile));
+		            while ((line = reader.readLine()) != null) 
+		            {
+		                // Comma = separator
+		                dataLine = line.split(",");
 
-	                // Add data components to single data entity, then add to array list
-	                dataList.add(new DataValue(dataLine[0], dataLine[1], dataLine[2], dataLine[3], dataLine[4],
-	                	dataLine[5], dataLine[6], dataLine[7], addSecondsToDate(Integer.parseInt(dataLine[0]))));
+		                // Add data components to single data entity, then add to array list
+		                dataList.add(new DataValue(dataLine[0], dataLine[1], dataLine[2], dataLine[3], dataLine[4],
+		                	dataLine[5], dataLine[6], dataLine[7], addSecondsToDate(Integer.parseInt(dataLine[0]))));
 
-	                // Retrieve a data item
-	                // DataValue cur_data = dataList.get(0);
-	                // System.out.println(cur_data.getTime());
+		                // Retrieve a data item
+		                // DataValue cur_data = dataList.get(0);
+		                // System.out.println(cur_data.getTime());
 
-	                System.out.println("Time: " + dataLine[0] + ", Type: " + dataLine[1] + ", Version: " + dataLine[2]
-	                	+ ", Counter: " + dataLine[3] + ", Via: " + dataLine[4] + ", Address: " + dataLine[5]
-	                	+ ", Status: " + dataLine[6] + ", Sensor Data: " + dataLine[7]);
+		                System.out.println("Time: " + dataLine[0] + ", Type: " + dataLine[1] + ", Version: " + dataLine[2]
+		                	+ ", Counter: " + dataLine[3] + ", Via: " + dataLine[4] + ", Address: " + dataLine[5]
+		                	+ ", Status: " + dataLine[6] + ", Sensor Data: " + dataLine[7]);
 
-	                System.out.println("Date: " + addSecondsToDate(Integer.parseInt(dataLine[0])) + "\n");
-	            }
+		                System.out.println("Date: " + addSecondsToDate(Integer.parseInt(dataLine[0])) + "\n");
+		            }
 
-	            // Show success dialog
-				//JOptionPane.showMessageDialog(new JPanel(),"File successfully opened!", "Success", JOptionPane.SUCCESS_MESSAGE);
+		            // Show success dialog
+					JOptionPane.showMessageDialog(new JFrame(), "File successfully opened!");
+				}
+				catch (FileNotFoundException e) {
+	            	e.printStackTrace();
+				}
+	       		catch (IOException e) {
+	            	e.printStackTrace();
+	       		}
 			}
-			catch (FileNotFoundException e) {
-            	e.printStackTrace();
-			}
-       		catch (IOException e) {
-            	e.printStackTrace();
-       		}
         }
 		else 
 			System.out.println("No file chosen!");
@@ -116,7 +129,7 @@ public class SensorData
 	public static void main(String[] args)
 	{
 		// Set time zone
-		dateFormat.setTimeZone(timeZone);
+		dateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
 
 		// Schedule a job for the event-dispatching thread: creating + showing the GUI.
         SwingUtilities.invokeLater(new Runnable() {
